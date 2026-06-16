79e4837b86b12a182a2f235a0b4062eca5154270
Cloud <clouddxai@gmail.com>
2026-06-16 15:29:30 +0900

    add reporting - screenshots video trace configured

--- playwright.config.js
+++ playwright.config.js
@@ -23,15 +23,28 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter:[
+    ['html', { open: 'never' }],
+  
+  // Shows results in terminal
+    ['list'] 
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     baseURL: 'https://the-internet.herokuapp.com',
+     // Screenshots — only when test fails
+  screenshot: 'on',
+  
+  // Video — only keep when test fails
+  video: 'retain-on-failure',
+  
+  // Trace — record on first retry
+  trace: 'on-first-retry',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+  
       launchOptions: {
     slowMo: 1000,
   },
